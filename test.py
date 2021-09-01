41bb663fb1a1e24a640e8cf4f4619ae2d205fec2
adirezky <musaharirezky@gmail.com>
2021-09-01 09:24:05 +0700

    ada

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/)
+
+@app.route('/')
 def index():
-   return "Hello World"
+    return "Hello World"
+
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
