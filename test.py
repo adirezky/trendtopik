a937b02f450fc5bf6ad0d02a64bd03e4ff1babce
adirezky <musaharirezky@gmail.com>
2021-09-08 17:27:08 +0700

    telah di ubah1

--- test.py
+++ test.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from nltk.corpus import stopwords
+from flask import Flask, jsonify, request, redirect, url_for
+from flask_restful import Resource, Api
+from flask_cors import CORS
+import pandas as pd
+from openpyxl import load_workbook
+import re
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+import string
+import nltk
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from sklearn.feature_extraction.text import CountVectorizer
+nltk.download('stopwords')
+nltk.download('punkt')
+
 
 app = Flask(__name__)
 
+api = Api(app)
 
-@app.route('/')
-def index():
-    return "Hello World"
+CORS(app)
 
 
-if __name__ == '__main__':
-    app.run()
+class TrendResource(Resource):
+    def get(self):
+        response = {"msg": "hallo"}
+        return response
+
+
+api.add_resource(TrendResource, "/api", methods=["GET"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
